32ba8b618156443b9bfc816398fdba4f17cc2924
Baixst <bawini97@yahoo.de>
2021-12-18 14:42:13 +0100

    logic and animation for checking/unchecking tasks

---
+++ Assets/Scripts/FadeObject.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeObject : MonoBehaviour
+{
+    public bool fadeOut = false;
+    public bool fadeIn = false;
+    public float fadeSpeed;
+
+    void Update()
+    {
+        if (fadeOut)
+        {
+            // fade the check box image
+            Color imageColor = gameObject.GetComponentInChildren<Image>().color;
+            float fadeAmount = imageColor.a - (fadeSpeed * Time.deltaTime);
+
+            imageColor = new Color(imageColor.r, imageColor.g, imageColor.b, fadeAmount);
+            gameObject.GetComponentInChildren<Image>().color = imageColor;
+
+            // fade the task text
+            Color textColor = gameObject.GetComponentInChildren<Text>().color;
+            fadeAmount = textColor.a - (fadeSpeed * Time.deltaTime);
+
+            textColor = new Color(textColor.r, textColor.g, textColor.b, fadeAmount);
+            gameObject.GetComponentInChildren<Text>().color = textColor;
+
+            // fade the checkbox marker
+            Color checkmarkColor = gameObject.GetComponent<Toggle>().graphic.color;
+            fadeAmount = checkmarkColor.a - (fadeSpeed * Time.deltaTime);
+
+            checkmarkColor = new Color(checkmarkColor.r, checkmarkColor.g, checkmarkColor.b, fadeAmount);
+            gameObject.GetComponent<Toggle>().graphic.color = checkmarkColor;
+
+            if (imageColor.a <= 0)
+            {
+                fadeOut = false;
+            }
+        }
+
+        if (fadeIn)
+        {
+            // fade in the checkbox image
+            Color imageColor = gameObject.GetComponentInChildren<Image>().color;
+            imageColor = new Color(imageColor.r, imageColor.g, imageColor.b, 1f);
+            gameObject.GetComponentInChildren<Image>().color = imageColor; 
+
+            // fade in the task text
+            Color textColor = gameObject.GetComponentInChildren<Text>().color;
+            textColor = new Color(textColor.r, textColor.g, textColor.b, 1f);
+            gameObject.GetComponentInChildren<Text>().color = textColor;
+
+            // fade in the checkbox marker
+            Color checkmarkColor = gameObject.GetComponent<Toggle>().graphic.color;
+            checkmarkColor = new Color(checkmarkColor.r, checkmarkColor.g, checkmarkColor.b, 1f);
+            gameObject.GetComponent<Toggle>().graphic.color = checkmarkColor;
+
+            fadeIn = false;
+        }
+    }
+}
--- Assets/Scripts/TaskManager.cs
+++ Assets/Scripts/TaskManager.cs
@@ -2,18 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.EventSystems;
-using TMPro;
 
 public class TaskManager : MonoBehaviour
 {
     public GameObject taskPrefab;
-    public Transform scrollViewContent;
+    private List<GameObject> tasks = new List<GameObject>();
+    private List<bool> toggleStati = new List<bool>();
+    public Transform activeScrollViewContent;
+    public Transform doneScrollViewContent;
+    private string taskName;
 
-    public void createNewTask(string str)
-    { 
-        GameObject newTask = Instantiate(taskPrefab);
-        newTask.transform.SetParent(scrollViewContent, false);
-        newTask.GetComponent<Task>().SetNameTo(str);
+    public void UpdateTaskName(string str)
+    {
+        taskName = str;
+    }
+
+    public void CreateNewTask()
+    {
+        if (taskName == null)   return;
+
+        if (taskName.Length > 0)
+        {
+            GameObject newTask = Instantiate(taskPrefab);
+            newTask.transform.SetParent(activeScrollViewContent, false);
+            newTask.GetComponentInChildren<Text>().text = taskName;
+            tasks.Add(newTask);
+            toggleStati.Add(newTask.GetComponent<Toggle>().isOn);
+        }
+    }
+
+    private IEnumerator ToggleTask(GameObject listElement)
+    {
+        yield return new WaitForSeconds(0.2f);
+        listElement.GetComponent<FadeObject>().fadeOut = true;
+        yield return new WaitForSeconds(0.35f);
+
+        if (listElement.GetComponent<Toggle>().isOn)
+        {
+            listElement.transform.SetParent(doneScrollViewContent, false);
+            listElement.GetComponent<FadeObject>().fadeIn = true;
+        }
+        else
+        {
+            listElement.transform.SetParent(activeScrollViewContent, false);
+            listElement.GetComponent<FadeObject>().fadeIn = true;
+        }
+    }
+
+    void Update()
+    {
+        if (tasks == null)  return;
+
+        for (int i = 0; i < tasks.Count; i++)
+        {
+            if (tasks[i].GetComponent<Toggle>().isOn != toggleStati[i])
+            {
+                StartCoroutine(ToggleTask(tasks[i]));
+                toggleStati[i] = !toggleStati[i];
+            }
+        }
     }
 }
