9a41103f3cd467d13496b9e826105abd0d427f97
Baixst <bawini97@yahoo.de>
2021-12-22 19:46:08 +0100

    dragging finally working

---
+++ Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragAndDrop : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    private RectTransform draggingObjectRectTransform;
+
+    private bool firstDrag = true;
+    private float startX;
+    private float startY;
+    private float startZ;
+
+    private void Awake()
+    {
+        draggingObjectRectTransform = transform as RectTransform;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (RectTransformUtility.ScreenPointToWorldPointInRectangle(draggingObjectRectTransform, eventData.position,
+            eventData.pressEventCamera, out var globalMousePosition))
+        {
+            var tempTransform = globalMousePosition;
+            tempTransform.y = startY;
+            tempTransform.z = startZ;
+            
+            if (tempTransform.x > startX)
+            {
+                tempTransform.x = startX;
+            }
+            draggingObjectRectTransform.transform.position = tempTransform;
+        }
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (firstDrag) 
+        {
+            startX = transform.position.x;
+            firstDrag = false;
+        }
+        startY = transform.position.y;
+        startZ = transform.position.z;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        // Delete Object or move it back
+        /* if (draggingObjectRectTransform.position.x < 50f)
+        {
+            gameObject.GetComponent<FadeObject>().fadeOut = true;
+        }
+        else
+        */
+        //{
+            StartCoroutine(MoveOverSeconds(gameObject, new Vector3 (startX, startY, startZ), 0.2f));
+        //}
+    }
+
+    public IEnumerator MoveOverSeconds (GameObject objectToMove, Vector3 endPosition, float seconds)
+    {
+        float elapsedTime = 0;
+        Vector3 startingPos = objectToMove.transform.position;
+        while (elapsedTime < seconds)
+        {
+            objectToMove.transform.position = Vector3.Lerp(startingPos, endPosition, (elapsedTime / seconds));
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        objectToMove.transform.position = endPosition;
+    }
+}
