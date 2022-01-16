84eb329c851cd32fae054cabb76e9c3ce6cb42c3
Maxoniar <max.angermueller@gmail.com>
2022-01-16 16:26:08 +0100

    small changes in ResultTracking

---
+++ Assets/Scripts/ResultTracker.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultTracker : MonoBehaviour
+{
+    public float timeTracker;
+    public Text textBox;        // just for testing
+    public List<float> results;
+    private int currentTaskIndex = 0;
+    private bool trackTime = false;
+    bool trackedTasksDone = false;
+    bool trackedTasksActivated = false;
+    bool trackedTasksDeleted = false;
+
+    void Start()
+    {
+        textBox.text = timeTracker.ToString("F2");
+    }
+
+    void Update()
+    {
+        if (trackTime)
+        {
+            timeTracker += Time.deltaTime;
+            textBox.text = timeTracker.ToString("F2");
+        }
+    }
+
+    public void StopTime()
+    {
+        trackTime = false;
+        results[currentTaskIndex] = timeTracker;
+        currentTaskIndex++;
+    }
+
+    public void TrackNextTime()
+    {
+        timeTracker = 0;
+        trackTime = true;
+    }
+}
